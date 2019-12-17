8e797dab101512c93c8f0e65fafe9c99c998d933
Jesse Steig <jsteig@alteryx.com>
2019-12-17 15:53:53 -0700

    Changing the repository to remove any db obfuscation.

--- hashmodels.go
+++ hashmodels.go
@@ -8,8 +8,8 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// SQLiteHashStore is a handler for wrapping the db pointer.
-type SQLiteHashStore struct {
+// HashRepository is a handler for wrapping the db pointer.
+type HashRepository struct {
 	db            *sql.DB
 	hashTableName string
 }
@@ -28,20 +28,20 @@ type TotalStats struct {
 }
 
 // NewHashStore creates an initialized HashStore
-func NewHashStore(db *sql.DB, hashTableName string) (*SQLiteHashStore, error) {
-	hashStore := SQLiteHashStore{
+func NewHashStore(db *sql.DB, hashTableName string) (*HashRepository, error) {
+	hashStore := HashRepository{
 		db:            db,
 		hashTableName: hashTableName,
 	}
 	err := hashStore.InitTables()
 	if err != nil {
-		return &SQLiteHashStore{}, err
+		return &HashRepository{}, err
 	}
 	return &hashStore, nil
 }
 
 // InitTables creates the schemas for the stored hashes returning any errors or nil on success.
-func (dataconnection *SQLiteHashStore) InitTables() error {
+func (dataconnection *HashRepository) InitTables() error {
 	query := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY NOT NULL, countID INTEGER, hashValue TEXT, hashTimeInMilliseconds INTEGER)", dataconnection.hashTableName)
 	_, err := dataconnection.db.Exec(query)
 	if err != nil {
@@ -52,7 +52,7 @@ func (dataconnection *SQLiteHashStore) InitTables() error {
 }
 
 // StoreHash stores a hashed string and the milliseconds to hash it returning an error on error or nil error on success.
-func (dataconnection *SQLiteHashStore) StoreHash(hash string, hashTimeInMilliseconds int) (int, error) {
+func (dataconnection *HashRepository) StoreHash(hash string, hashTimeInMilliseconds int) (int, error) {
 	query := fmt.Sprintf("INSERT INTO %s (hashValue, hashTimeInMilliseconds, countID) VALUES (?, ?, (SELECT COUNT(countID) + 1 FROM %s))", dataconnection.hashTableName, dataconnection.hashTableName)
 	insert, err := dataconnection.db.Prepare(query)
 	if err != nil {
@@ -69,7 +69,7 @@ func (dataconnection *SQLiteHashStore) StoreHash(hash string, hashTimeInMillisec
 }
 
 // GetHashStat returns the HashStat for a stored countID or an error on error.
-func (dataconnection *SQLiteHashStore) GetHashStat(countID int) (HashStat, error) {
+func (dataconnection *HashRepository) GetHashStat(countID int) (HashStat, error) {
 	query := fmt.Sprintf("SELECT hashValue, hashTimeInMilliseconds from %s where countID=?", dataconnection.hashTableName)
 	rows, queryError := dataconnection.db.Query(query, countID)
 	if queryError != nil {
@@ -85,7 +85,7 @@ func (dataconnection *SQLiteHashStore) GetHashStat(countID int) (HashStat, error
 }
 
 // GetTotalStats gets the total stats for all hashes.
-func (dataconnection *SQLiteHashStore) GetTotalStats() (TotalStats, error) {
+func (dataconnection *HashRepository) GetTotalStats() (TotalStats, error) {
 	query := fmt.Sprintf("SELECT COUNT(countID), AVG(hashTimeInMilliseconds from %s", dataconnection.hashTableName)
 	rows, queryError := dataconnection.db.Query(query)
 	if queryError != nil {
@@ -101,7 +101,7 @@ func (dataconnection *SQLiteHashStore) GetTotalStats() (TotalStats, error) {
 }
 
 // ClearStore clears all data for the hash tables.
-func (dataconnection *SQLiteHashStore) ClearStore() error {
+func (dataconnection *HashRepository) ClearStore() error {
 	query := fmt.Sprintf("DROP TABLE IF EXISTS %s", dataconnection.hashTableName)
 	_, err := dataconnection.db.Exec(query)
 	if err != nil {
@@ -111,7 +111,7 @@ func (dataconnection *SQLiteHashStore) ClearStore() error {
 }
 
 // Close should be called to clear all data.
-func (dataconnection *SQLiteHashStore) Close() error {
+func (dataconnection *HashRepository) Close() error {
 	err := dataconnection.db.Close()
 	if err != nil {
 		return err
